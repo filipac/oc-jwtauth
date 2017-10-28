aa1312bc71d4422cde4401245ab0cb4fd3727ae5
Filip Iulian Pacurar <hello@filipac.net>
2017-10-28 16:19:31 +0300

    refactor service provider

--- Plugin.php
+++ Plugin.php
@@ -10,46 +10,66 @@ use Vdomah\JWTAuth\Classes\Guard;
 
 class Plugin extends PluginBase
 {
+    public $elevated = true;
+
     /**
      * @var array   Require the RainLab.Blog plugin
      */
     public $require = ['RainLab.User'];
 
-    public function registerComponents()
+    public function register()
     {
-    }
+        $this->registerJwtPackage();
 
-    public function registerSettings()
-    {
+        $this->registerErrorHandler();
+
+        $this->registerSingletons();
+
+        $this->registerFacades();
     }
 
     public function boot()
     {
-        App::error(function($exception) {
-            if($exception instanceof UnauthorizedHttpException) {
-                return ['error' => 'unauthorised'];
-            }
-        });
-        App::register(\Vdomah\JWTAuth\Classes\JWTAuthServiceProvider::class);
+        $this->makebinds();
+        $this->app['router']->middleware('jwt.auth', \Tymon\JWTAuth\Http\Middleware\Authenticate::class);
+        $this->app['router']->middleware('jwt.refresh', \Tymon\JWTAuth\Http\Middleware\RefreshToken::class);
+        $this->app['router']->middleware('jwt.refresh.auth', AuthenticateAndRenew::class);
+    }
 
-        $this->app->bind(\Illuminate\Contracts\Auth\Guard::class, 'user.auth');
-
-        $facade = AliasLoader::getInstance();
-        $facade->alias('JWTAuth', '\Tymon\JWTAuth\Facades\JWTAuth');
-        $facade->alias('JWTFactory', '\Tymon\JWTAuth\Facades\JWTFactory');
-
-        App::singleton(Guard::class, function() {
+    private function registerSingletons()
+    {
+        App::singleton(Guard::class, function () {
             return \Vdomah\JWTAuth\Classes\AuthManager::instance();
         });
 
         App::singleton('auth', function ($app) {
             return app(Guard::class);
         });
+    }
 
+    private function registerFacades()
+    {
+        $facade = AliasLoader::getInstance();
+        $facade->alias('JWTAuth', '\Tymon\JWTAuth\Facades\JWTAuth');
+        $facade->alias('JWTFactory', '\Tymon\JWTAuth\Facades\JWTFactory');
+    }
 
+    private function registerJwtPackage()
+    {
+        App::register(\Vdomah\JWTAuth\Classes\JWTAuthServiceProvider::class);
+    }
 
-        $this->app['router']->middleware('jwt.auth', \Tymon\JWTAuth\Http\Middleware\Authenticate::class);
-        $this->app['router']->middleware('jwt.refresh', \Tymon\JWTAuth\Http\Middleware\RefreshToken::class);
-        $this->app['router']->middleware('jwt.refresh.auth', AuthenticateAndRenew::class);
+    private function registerErrorHandler()
+    {
+        App::error(function ($exception) {
+            if ($exception instanceof UnauthorizedHttpException) {
+                return ['error' => 'unauthorised'];
+            }
+        });
+    }
+
+    private function makebinds()
+    {
+        $this->app->bind(\Illuminate\Contracts\Auth\Guard::class, 'user.auth');
     }
 }
